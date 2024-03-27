5669750a4764af9dd3ad9a912d2e795ba31600ab
Bob <bobmarconi22@gmail.com>
2024-03-27 19:29:02 -0400

    phase 1 complete

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,13 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+
+const apiRouter = require("./api");
+
+router.use("/api", apiRouter);
 
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
-    const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
-    res.status(200).json({
-      'XSRF-Token': csrfToken
-    });
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    "XSRF-Token": csrfToken,
   });
+});
+
+router.post("/test", function (req, res) {
+  res.json({ requestBody: req.body });
+});
 
 module.exports = router;
