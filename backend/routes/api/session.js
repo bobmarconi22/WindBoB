cdea3c8be0ff713a46bfd8bf6e46219f5811d655
Bob <bobmarconi22@gmail.com>
2024-03-29 11:26:25 -0400

    firstName, lastName refactor

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -7,65 +7,77 @@ const { User } = require("../../db/models");
 
 const router = express.Router();
 
-router.post(
-    '/',
-    async (req, res, next) => {
-      const { credential, password } = req.body;
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
 
-      const user = await User.unscoped().findOne({
-        where: {
-          [Op.or]: {
-            username: credential,
-            email: credential
-          }
-        }
-      });
+const validateLogin = [
+  check("email")
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage("Please provide a valid email."),
+  check("username")
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage("Please provide a valid username."),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a password."),
+  handleValidationErrors,
+];
+router.post("/", validateLogin, async (req, res, next) => {
+  const { email, username, password } = req.body;
 
-      if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
-        const err = new Error('Login failed');
-        err.status = 401;
-        err.title = 'Login failed';
-        err.errors = { credential: 'The provided credentials were invalid.' };
-        return next(err);
-      }
+  console.log("----------->", email, username, password);
+  const user = await User.unscoped().findOne({
+    where: {
+      [Op.or]: {
+        username: username,
+        email: email,
+      },
+    },
+  });
+  if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
+    const err = new Error("Login failed");
+    err.status = 401;
+    err.title = "Login failed";
+    err.errors = { credential: "The provided credentials were invalid." };
+    return next(err);
+  }
 
-      const safeUser = {
-        id: user.id,
-        email: user.email,
-        username: user.username,
-      };
+  const safeUser = {
+    id: user.id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    username: user.username,
+  };
 
-      await setTokenCookie(res, safeUser);
+  await setTokenCookie(res, safeUser);
 
-      return res.json({
-        user: safeUser
-      });
-    }
-  );
+  return res.json({
+    user: safeUser,
+  });
+});
 
-  router.delete(
-    '/',
-    (_req, res) => {
-      res.clearCookie('token');
-      return res.json({ message: 'success' });
-    }
-  );
+router.delete("/", (_req, res) => {
+  res.clearCookie("token");
+  return res.json({ message: "success" });
+});
 
-  router.get(
-    '/',
-    (req, res) => {
-      const { user } = req;
-      if (user) {
-        const safeUser = {
-          id: user.id,
-          email: user.email,
-          username: user.username,
-        };
-        return res.json({
-          user: safeUser
-        });
-      } else return res.json({ user: null });
-    }
-  );
+router.get("/", (req, res) => {
+  const { user } = req;
+  if (user) {
+    const safeUser = {
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      username: user.username,
+    };
+    return res.json({
+      user: safeUser,
+    });
+  } else return res.json({ user: null });
+});
 
-  module.exports = router;
+module.exports = router;
