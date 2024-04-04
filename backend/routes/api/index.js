37a0592e59ef73e1f0476bf97f99793c2bc62125
Bob <bobmarconi22@gmail.com>
2024-04-04 10:12:48 -0400

    get-bookings/current ready to test

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -45,6 +45,7 @@ const sessionRouter = require("./session.js");
 const usersRouter = require("./users.js");
 const spotsRouter = require("./spots.js");
 const reviewsRouter = require("./reviews.js");
+const bookingsRouter = require("./bookings.js");
 
 const { restoreUser } = require("../../utils/auth.js");
 
@@ -54,12 +55,10 @@ const { restoreUser } = require("../../utils/auth.js");
 router.use(restoreUser);
 
 router.use("/session", sessionRouter);
-
 router.use("/users", usersRouter);
-
 router.use("/spots", spotsRouter);
-
 router.use("/reviews", reviewsRouter);
+router.use("/bookings", bookingsRouter);
 
 router.post("/test", (req, res) => {
   res.json({ requestBody: req.body });
