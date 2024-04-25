750870f11c9f76de6887cb8b20c36948862638e2
Bob <bobmarconi22@gmail.com>
2024-04-25 19:31:46 -0400

    seeder reset

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -71,7 +71,7 @@ module.exports = (sequelize, DataTypes) => {
         },
       },
       description: {
-        type: DataTypes.STRING,
+        type: DataTypes.TEXT,
         allowNull: false,
       },
       price: {
